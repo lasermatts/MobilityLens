b5946ec16cf8a493419811f93f240a9315c15c99
Matt <mattpuchalski5@gmail.com>
2023-10-20 20:45:59 -0400

    python script that writes to jpg

---
+++ camera_module/capture_script.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+from datetime import datetime
+
+# Set the default directory to "/camera_module/image_output/"
+DEFAULT_OUTPUT_DIR = '/camera_module/image_output/'
+
+def ensure_dir_exists(dir_path):
+    """
+    Ensure the specified directory exists. If it doesn't, create it.
+    """
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+def capture_image(output_dir=DEFAULT_OUTPUT_DIR, filename_prefix='image'):
+    """
+    Capture an image using libcamera.
+    The image is saved in the specified directory with a filename that's a combination of the given prefix and the current timestamp.
+    """
+    # Ensure the output directory exists
+    ensure_dir_exists(output_dir)
+
+    # Get the current timestamp
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    
+    # Construct the filename
+    filename = f"{filename_prefix}_{timestamp}.jpg"
+    output_path = os.path.join(output_dir, filename)
+    
+    # Capture the image
+    cmd = ['libcamera-still', '-o', output_path]
+    subprocess.run(cmd, check=True)
+    print(f"Image saved to: {output_path}")
+
+if __name__ == "__main__":
+    # Example usage: capturing an image in the default directory with a prefix 'capture'
+    capture_image(filename_prefix='capture')
---
+++ camera_module/image_output/image_output.md
@@ -0,0 +1,3 @@
+# IMAGE_OUTPUT
+
+Here's where we store all of the stills taken temporarily. Eventually we'll get a more clever filesystem in place to handle moving the images around.
